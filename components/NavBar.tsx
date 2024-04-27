ea4c361431a331b1bc18b4f0b166c000140aa243
Caio H Perlin <caiohperlin@gmail.com>
2024-04-27 10:59:56 -0400

    feat: add animation to mobile menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,8 +5,8 @@ const NavBar = () => {
 	const [isVisible, setVisibility] = useState(false);
 
 	return (
-		<header className="text-2xl flex flex-col w-full pb-1 shadow-sm shadow-black bg-gradient-to-r from-emerald-500 via-cyan-700 to-sky-950 bg-emerald-600 border-b-indigo-600">
-			<div className="flex w-full flex-row">
+		<header className="text-2xl -z-10 flex flex-col w-full pb-1 shadow-sm shadow-black bg-gradient-to-r from-emerald-500 via-cyan-700 to-sky-950 bg-emerald-600">
+			<div className="flex z-10 w-full flex-row">
 				<div className="flex flex-row basis-1/5 md:hidden justify-center bg-slate-800 items-center lg:items-baseline gap-3 px-10 py-8">
 					<button onClick={() => setVisibility(!isVisible)}>
 						<i className="fa-solid fa-bars"></i>
@@ -32,9 +32,10 @@ const NavBar = () => {
 					</div>
 				</div>
 			</div>
-			{isVisible ? (
-				<nav className="basis-full bg-slate-800 pb-8 flex flex-col md:hidden">
-					<h1 className="text-center pb-6">
+			{/*MOBILE MENU*/}
+			<div className={`${isVisible ? 'translate-y-20' : '-translate-y-full'} absolute ease-in-out duration-200 flex flex-col w-full pb-1 shadow-sm shadow-black bg-gradient-to-r from-emerald-500 via-cyan-700 to-sky-950 bg-emerald-600`}>
+				<nav className={`z-0  basis-full bg-slate-800 pt-4 pb-2 flex w-full flex-col md:hidden`}>
+					<h1 className="text-center pb-4">
 						caiohygino<span className="font-bold">.software</span>
 					</h1>
 					<a className="m-2 py-2 px-4 bg-slate-700 flex flex-row justify-between items-center rounded-md" href="#">
@@ -54,9 +55,7 @@ const NavBar = () => {
 						<i className="fa-solid fa-folder-open"></i>
 					</a>
 				</nav>
-			) : (
-				''
-			)}
+			</div>
 		</header>
 	);
 };
